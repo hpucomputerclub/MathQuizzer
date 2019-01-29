6d6d9d5730f751392c6d1f998e7a62c037e47587
Student <student@atmlc-imac-15.local>
2019-01-28 18:27:31 -1000

    Copied template

---
+++ MathQuizzer/MathQuizzer/Public/TaylorHiga.swift
@@ -0,0 +1,15 @@
+//
+//  TaylorHiga.swift
+//  MathQuizzer
+//
+//  Created by Student on 1/28/19.
+//  Copyright © 2019 HPUComputerClub. All rights reserved.
+//
+
+import UIKit
+
+extension Main{
+    /*Insert Assignments In HERE*/
+    
+    
+}
