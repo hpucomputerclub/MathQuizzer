e168819baa091108f0b0331130df38d587f9b291
Student <student@atmlc-imac-10.hpu.edu>
2019-02-25 18:05:07 -1000

    Created calculate right answer method

---
+++ MathQuizzer/MathQuizzer/Public/EdwardFlores.swift
@@ -0,0 +1,17 @@
+//
+//  EdwardFlores.swift
+//  MathQuizzer
+//
+//  Created by Student on 2/25/19.
+//  Copyright © 2019 HPUComputerClub. All rights reserved.
+//
+
+import UIKit
+
+extension Main{
+    /*Insert Assignments In HERE*/
+    
+    func calcuateRightAnswer(){
+        correctAnswer = firstNumber+secondNumber
+    }
+}
