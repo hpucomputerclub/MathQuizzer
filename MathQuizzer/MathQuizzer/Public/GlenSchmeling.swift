dd612f92a2f96d97fd4269cb5e0bb558bbba1aac
Glen Schmeling <Macx@Glens-MacBook-Pro.local>
2019-03-05 15:20:19 -1000

    Working

--- MathQuizzer/MathQuizzer/Public/GlenSchmeling.swift
+++ MathQuizzer/MathQuizzer/Public/GlenSchmeling.swift
@@ -21,6 +21,40 @@ import UIKit
 
 extension Main{
     /*Insert Assignments In HERE*/
+    /*==============================
+     Title: Update User Answer
+     Input: None
+     Return: None
+     
+     Description:
+     1. This method should update the userAnswer variable
+     2. Use enterAnswer as shown below
+    */
+    
+    func glenEnterAnswer(){
+        enterAnswer = { (_ text: String) in
+            
+            if let num = Int(text){//Int type - impossible to be nil
+                //parse was good
+                self.userAnswer = num;
+            }else{
+                //parse failed
+                self.popUp(title: "Error", message: "Please try again, that number is not valid.")
+                
+            }
+            
+            /*closure is where you save a method inside a variable  look at line  #45 in main for declaration of closure
+             
+             line 45: var enterAnswer: (_ text: String) -> () = {_ in }   input method (parameters) output method (return type), actual method
+             every time you access to a method of variable use the self.
+             
+             let num2 = Int(text)//Int? <- Optional type
+             print("Is num2 null: \(num2 == nil)")
+             */
+            
+        }
+    }
     
     
 }
+
